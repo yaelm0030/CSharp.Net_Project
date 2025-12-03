006eab51becf5e7dfc8011a21cf6bcbe36ec796c
yaelm0030 <yaelm0030@gmail.com>
2025-12-04 00:01:34 +0200

    Fixing and Ending Stage 1

--- DotNet2025_7420_6348/DalFacade/obj/Debug/net8.0/DalFacade.AssemblyInfo.cs
+++ DotNet2025_7420_6348/DalFacade/obj/Debug/net8.0/DalFacade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935207112209b4b7367018f5f8b84ebbdd86fb1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DotNet2025_7420_6348/DalList/obj/Debug/net8.0/DalList.AssemblyInfo.cs
+++ DotNet2025_7420_6348/DalList/obj/Debug/net8.0/DalList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935207112209b4b7367018f5f8b84ebbdd86fb1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
